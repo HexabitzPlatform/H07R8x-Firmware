c2acaf79d8f91faf85133ac291b470270de34d24
Abdulrhman_Battikh <abdulrhmanbattikh123@gmail.com>
2024-03-11 07:03:53 -0700

    MAX9704_Amplifier APIs

--- Core/Inc/MAX9704_Amplifier.h
+++ Core/Inc/MAX9704_Amplifier.h
@@ -1,12 +1,21 @@
 /*
- * MAX9704_Amplifier.h
- *
+ * MAX9704_Amplifier.c
+ * Description: MAX9704_Amplifier source file
  *  Created on: Mar 11, 2024
- *      Author: Abdulrhman_Battikh
+ *      Author: Abdulrahman Battikh @ Hexabitz
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Hexabitz.
+ * All rights reserved.
+ *
+ ******************************************************************************
  */
 
 #ifndef INC_MAX9704_AMPLIFIER_H_
 #define INC_MAX9704_AMPLIFIER_H_
+
+#include "main.h"
 
 typedef enum{
 	SWITCHING_MODE_670KHZ = 0,
@@ -15,5 +24,53 @@ typedef enum{
 	SWITCHING_MODE_AROUND_670KHZ
 }Switching_Modes;
 
+typedef enum{
+	GAIN_MODE_29dB = 0,
+	GAIN_MODE_19dB,
+	GAIN_MODE_16dB,
+	GAIN_MODE_13dB
+}Gain_Modes;
+
+typedef enum{
+	SHUTDOWN_ENABLE = 0,
+	SHUTDOWN_DISABLE
+}Shutdown_Modes;
+
+typedef enum{
+	MUTE_ENABLE = 0,
+	MUTE_DISABLE
+}Mute_Modes;
+
+/* Amplifier Switching Mode */
+/*
+ * @brief  :Audio switching frequency control.
+ * @param1 :Switching Modes
+ * @retval :Nothing
+ */
+void AmpSwitchingMode(Switching_Modes mode);
+
+/* Amplifier Gain */
+/*
+ * @brief  :Audio level control.
+ * @param1 :Gain Modes
+ * @retval :Nothing
+ */
+void AmpGain(Gain_Modes mode);
+
+/* Amplifier Shutdown */
+/*
+ * @brief  :Amplifier Shutdown.
+ * @param1 :Shutdown Modes
+ * @retval :Nothing
+ */
+void AmpShutdown(Shutdown_Modes mode);
+
+/* Amplifier Mute */
+/*
+ * @brief  :Aduio Mute.
+ * @param1 :Mute Modes
+ * @retval :Nothing
+ */
+void AmpMute(Mute_Modes mode);
 
 #endif /* INC_MAX9704_AMPLIFIER_H_ */
--- Core/Src/MAX9704_Amplifier.c
+++ Core/Src/MAX9704_Amplifier.c
@@ -1,9 +1,99 @@
 /*
  * MAX9704_Amplifier.c
- *
+ * Description: MAX9704_Amplifier source file
  *  Created on: Mar 11, 2024
- *      Author: Abdulrhman_Battikh
+ *      Author: Abdulrahman Battikh @ Hexabitz
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Hexabitz.
+ * All rights reserved.
+ *
+ ******************************************************************************
  */
-
 #include "MAX9704_Amplifier.h"
 
+void AmpSwitchingMode(Switching_Modes mode)
+{
+	switch(mode)
+	{
+		case SWITCHING_MODE_670KHZ:
+			HAL_GPIO_WritePin(AMP_FS1_GPIO_Port, AMP_FS1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(AMP_FS2_GPIO_Port, AMP_FS2_Pin, GPIO_PIN_RESET);
+			break;
+		case SWITCHING_MODE_940KHZ:
+			HAL_GPIO_WritePin(AMP_FS1_GPIO_Port, AMP_FS1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(AMP_FS2_GPIO_Port, AMP_FS2_Pin, GPIO_PIN_SET);
+			break;
+		case SWITCHING_MODE_470KHZ:
+			HAL_GPIO_WritePin(AMP_FS1_GPIO_Port, AMP_FS1_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(AMP_FS2_GPIO_Port, AMP_FS2_Pin, GPIO_PIN_RESET);
+			break;
+		case SWITCHING_MODE_AROUND_670KHZ:
+			HAL_GPIO_WritePin(AMP_FS1_GPIO_Port, AMP_FS1_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(AMP_FS2_GPIO_Port, AMP_FS2_Pin, GPIO_PIN_SET);
+			break;
+		/* default mode is SWITCHING_MODE_670KHZ */
+		default:
+			HAL_GPIO_WritePin(AMP_FS1_GPIO_Port, AMP_FS1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(AMP_FS2_GPIO_Port, AMP_FS2_Pin, GPIO_PIN_RESET);
+	}
+}
+
+void AmpGain(Gain_Modes mode)
+{
+	switch(mode)
+	{
+		case GAIN_MODE_29dB:
+			HAL_GPIO_WritePin(AMP_GAIN1_GPIO_Port, AMP_GAIN1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(AMP_GAIN2_GPIO_Port, AMP_GAIN2_Pin, GPIO_PIN_RESET);
+			break;
+		case GAIN_MODE_19dB:
+			HAL_GPIO_WritePin(AMP_GAIN1_GPIO_Port, AMP_GAIN1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(AMP_GAIN2_GPIO_Port, AMP_GAIN2_Pin, GPIO_PIN_SET);
+			break;
+		case GAIN_MODE_16dB:
+			HAL_GPIO_WritePin(AMP_GAIN1_GPIO_Port, AMP_GAIN1_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(AMP_GAIN2_GPIO_Port, AMP_GAIN2_Pin, GPIO_PIN_RESET);
+			break;
+		case GAIN_MODE_13dB:
+			HAL_GPIO_WritePin(AMP_GAIN1_GPIO_Port, AMP_GAIN1_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(AMP_GAIN2_GPIO_Port, AMP_GAIN2_Pin, GPIO_PIN_SET);
+			break;
+		/* default mode is GAIN_MODE_29dB */
+		default:
+			HAL_GPIO_WritePin(AMP_GAIN1_GPIO_Port, AMP_GAIN1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(AMP_GAIN2_GPIO_Port, AMP_GAIN2_Pin, GPIO_PIN_RESET);
+	}
+}
+void AmpShutdown(Shutdown_Modes mode)
+{
+	switch(mode)
+	{
+		case SHUTDOWN_ENABLE:
+			HAL_GPIO_WritePin(AMP_SHUTDOWN_GPIO_Port, AMP_SHUTDOWN_Pin, GPIO_PIN_RESET);
+			break;
+		case SHUTDOWN_DISABLE:
+			HAL_GPIO_WritePin(AMP_SHUTDOWN_GPIO_Port, AMP_SHUTDOWN_Pin, GPIO_PIN_SET);
+			break;
+		/* default mode is SHUTDOWN_DISABLE */
+		default:
+			HAL_GPIO_WritePin(AMP_SHUTDOWN_GPIO_Port, AMP_SHUTDOWN_Pin, GPIO_PIN_SET);
+	}
+}
+
+void AmpMute(Mute_Modes mode)
+{
+	switch(mode)
+	{
+		case MUTE_ENABLE:
+			HAL_GPIO_WritePin(AMP_MUTE_GPIO_Port, AMP_MUTE_Pin, GPIO_PIN_RESET);
+			break;
+		case MUTE_DISABLE:
+			HAL_GPIO_WritePin(AMP_MUTE_GPIO_Port, AMP_MUTE_Pin, GPIO_PIN_SET);
+			break;
+		/* default mode is MUTE_DISABLE */
+		default:
+			HAL_GPIO_WritePin(AMP_MUTE_GPIO_Port, AMP_MUTE_Pin, GPIO_PIN_SET);
+	}
+}
