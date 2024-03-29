a034a3d371c2a0e60cf67b000cd5441039ad794f
Abdulrhman_Battikh <abdulrhmanbattikh123@gmail.com>
2024-03-30 02:01:48 +0300

    Adding Error_Handler() declaration

--- H07R8/H07R8_inputs.h
+++ H07R8/H07R8_inputs.h
@@ -30,7 +30,7 @@ extern void ReadADCChannel(uint8_t Port,char *side,float *ADC_Value);
 extern void ADCSelectChannel(uint8_t ADC_port,char *side);
 extern float GetReadPrecentage(uint8_t port,float *precentageValue);
 extern void Deinit_ADC_Channel(uint8_t port);
-
+extern void Error_Handler(void);
 /*-----------------------------------------------------------*/
 
 /************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
